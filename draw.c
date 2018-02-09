50875b381d95f6bad8b2727d94fe000b480006cf
Gian Tricarico <gtricarico@stuy.edu>
2018-02-08 23:46:41 -0500

    make vars for line algo

---
+++ draw.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ml6.h"
+#include "display.h"
+#include "draw.h"
+
+//Insert your line algorithm here
+void draw_line(int x0, int y0, int x1, int y1, screen s, color c) {
+  int a = y1 - y0; // delta y
+  int b = x0 - x1; // negative delta x
+  float m = -a/b; // slope
+
+  if (m 
+}
